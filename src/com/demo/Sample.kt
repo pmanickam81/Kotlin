5f0c05104991c09a57bfaada9d9b373196b59708
Manickam P <pmanickam81@gmail.com>
2018-06-17 16:01:43 +0530

    Kotlin - function expression added

--- src/com/demo/Sample.kt
+++ src/com/demo/Sample.kt
@@ -3,4 +3,7 @@ package com.demo
 fun main(args: Array<String>) {
     val str:String ="Welcome to Kotlin"
     println("Hello World, $str")
+    println("Sample function Expression = ${sum(10,20)}")
 }
+
+fun sum(a:Int, b:Int) = a+b
